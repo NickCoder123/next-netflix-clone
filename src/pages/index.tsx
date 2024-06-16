d94cde366a4fd96928ad2f72c0ebdf5ae7b27ce8
Nikunj Singh <nikunjmehra935@gmail.com>
2024-06-16 13:22:42 +0530

    auth screen ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <>
+    <div className="h-full">
       <h1 className="text-3xl text-green-600 font-bold underline">
         Netflix Clone
       </h1>
-    </>
+    </div>
   );
 }
