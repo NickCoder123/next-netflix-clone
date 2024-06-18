d622e3912c70a85fb67a2d237d19716cc0d5db67
Nikunj Singh <nikunjmehra935@gmail.com>
2024-06-18 08:47:20 +0530

    movie list w/ movie cards, movies endpoint

---
+++ src/pages/api/movies/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { db } from "~/db";
+import serverAuth from "~/lib/serverAuth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    await serverAuth(req);
+
+    const movies = await db.movie.findMany();
+
+    return res.status(200).json(movies);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).end();
+  }
+}
